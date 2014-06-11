4e37e420a214fd18f8264ecb57344425861b9469
Kostas K <mpokwsths@gmail.com>
2014-06-11 15:32:49 +0300

    cpufreq: ondemand: Remove redundant return statement
    After commit dfa5bb622555 (cpufreq: ondemand: Change the calculation
    of target frequency), this return statement is no longer needed.
    
    [mpokwsths: Adapted to Android's (kernel 3.4) ondemand governor]
    
    Reported-by: Henrik Nilsson <Karl.Henrik.Nilsson@gmail.com>
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Kostas K <mpokwsths@gmail.com>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -868,7 +868,6 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 				return;
 			}
 		}
-		return;
 	} else {
 		/* Calculate the next frequency proportional to load */
 		unsigned int freq_next;
