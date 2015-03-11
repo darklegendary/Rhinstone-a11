cc1d1c33def5e760c14aeb9a4d7477235ddd9f02
UpInTheAir <upintheair.xda@gmail.com>
2015-03-11 18:02:02 +0700

    mm: Adaptive Dirty Background Ratio & Dirty Ratio for suspend/resume
    Adaptively set the dirty_background_ratio and vm_dirty_ratio values during resume (screen on), and fixed values during suspend (screen off).
    
    The POWERSUSPEND driver is required for screen suspend/resume functions,
    otherwise both default to the resume value.
    
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>
    
    Conflicts:
    	mm/page-writeback.c

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -69,10 +69,13 @@ static long ratelimit_pages = 32;
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-#ifdef CONFIG_ZEN_INTERACTIVE
-int dirty_background_ratio = 20;
+#if defined(CONFIG_ADAPTIVE_DIRTY_BACKGROUND_RATIO) || defined(CONFIG_ZEN_INTERACTIVE)
+#define DEFAULT_DIRTY_BACKGROUND_RATIO 20
+int dirty_background_ratio, resume_dirty_background_ratio;
+#define DEFAULT_DIRTY_SUSPEND_BACKGROUND_RATIO 60
+int dirty_suspend_background_ratio, suspend_dirty_background_ratio;
 #else
-int dirty_background_ratio = 10;
+int dirty_background_ratio = 20;
 #endif
 
 /*
@@ -90,10 +93,13 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-#ifdef CONFIG_ZEN_INTERACTIVE
-int vm_dirty_ratio = 50;
+#if defined(CONFIG_ADAPTIVE_VM_DIRTY_RATIO) || defined(CONFIG_ZEN_INTERACTIVE)
+#define DEFAULT_VM_DIRTY_RATIO 50
+int vm_dirty_ratio, resume_vm_dirty_ratio;
+#define DEFAULT_VM_SUSPEND_DIRTY_RATIO 95
+int vm_suspend_dirty_ratio, suspend_vm_dirty_ratio;
 #else
-int vm_dirty_ratio = 20;
+int vm_dirty_ratio = 50;
 #endif
 
 /*
@@ -1695,6 +1701,52 @@ static struct notifier_block __cpuinitdata ratelimit_nb = {
 	.next		= NULL,
 };
 
+#ifdef CONFIG_ADAPTIVE_DIRTY_BACKGROUND_RATIO
+static void dbackground_power_suspend(struct power_suspend *handler)
+{
+	if (dirty_suspend_background_ratio != resume_dirty_background_ratio)
+		resume_dirty_background_ratio = dirty_suspend_background_ratio;
+
+	dirty_suspend_background_ratio = suspend_dirty_background_ratio;
+}
+
+static void dbackground_power_resume(struct power_suspend *handler)
+{
+	if (dirty_suspend_background_ratio != suspend_dirty_background_ratio)
+		suspend_dirty_background_ratio = dirty_suspend_background_ratio;
+
+	dirty_suspend_background_ratio = resume_dirty_background_ratio;
+}
+
+static struct power_suspend dbackground_suspend = {
+	.suspend = dbackground_power_suspend,
+	.resume = dbackground_power_resume,
+};
+#endif
+
+#ifdef CONFIG_ADAPTIVE_VM_DIRTY_RATIO
+static void dratio_power_suspend(struct power_suspend *handler)
+{
+	if (vm_suspend_dirty_ratio != resume_vm_dirty_ratio)
+		resume_vm_dirty_ratio = vm_suspend_dirty_ratio;
+
+	vm_suspend_dirty_ratio = suspend_vm_dirty_ratio;
+}
+
+static void dratio_power_resume(struct power_suspend *handler)
+{
+	if (vm_suspend_dirty_ratio != suspend_vm_dirty_ratio)
+		suspend_vm_dirty_ratio = vm_suspend_dirty_ratio;
+
+	vm_suspend_dirty_ratio = resume_vm_dirty_ratio;
+}
+
+static struct power_suspend dratio_suspend = {
+	.suspend = dratio_power_suspend,
+	.resume = dratio_power_resume,
+};
+#endif
+
 #ifdef CONFIG_DYNAMIC_PAGE_WRITEBACK
 /*
  * Sets the dirty page writebacks interval for suspended system
@@ -1765,6 +1817,24 @@ static struct power_suspend dirty_suspend = {
 void __init page_writeback_init(void)
 {
 	int shift;
+
+#ifdef CONFIG_ADAPTIVE_DIRTY_BACKGROUND_RATIO
+	dirty_background_ratio = resume_dirty_background_ratio =
+		DEFAULT_DIRTY_BACKGROUND_RATIO;
+	dirty_suspend_background_ratio = suspend_dirty_background_ratio =
+		DEFAULT_DIRTY_SUSPEND_BACKGROUND_RATIO;
+
+	register_power_suspend(&dbackground_suspend);
+#endif
+
+#ifdef CONFIG_ADAPTIVE_VM_DIRTY_RATIO
+	vm_dirty_ratio = resume_vm_dirty_ratio =
+		DEFAULT_VM_DIRTY_RATIO;
+	vm_suspend_dirty_ratio = suspend_vm_dirty_ratio =
+		DEFAULT_VM_SUSPEND_DIRTY_RATIO;
+
+	register_power_suspend(&dratio_suspend);
+#endif
 
 	dirty_expire_interval = resume_dirty_expire_interval =
 		DEFAULT_DIRTY_EXPIRE_INTERVAL;
