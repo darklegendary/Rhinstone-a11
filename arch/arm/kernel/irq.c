9e6c70fc6cf7a39dad3811ee9947e4d723beee02
pattyboi <patpat1325179@gmail.com>
2015-04-07 22:31:44 -0400

    remove some more debug
    Conflicts:
    	arch/arm/Makefile

--- arch/arm/kernel/irq.c
+++ arch/arm/kernel/irq.c
@@ -114,7 +114,6 @@ void handle_IRQ(unsigned int irq, struct pt_regs *regs)
 {
 	struct pt_regs *old_regs = set_irq_regs(regs);
 
-	perf_mon_interrupt_in();
 	irq_enter();
 
 	/*
@@ -134,7 +133,6 @@ void handle_IRQ(unsigned int irq, struct pt_regs *regs)
 
 	irq_exit();
 	set_irq_regs(old_regs);
-	perf_mon_interrupt_out();
 }
 
 /*
