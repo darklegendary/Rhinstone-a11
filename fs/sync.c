f65282a1572740d4a5d602363ae16e481c40250e
pattyboi <patpat1325179@gmail.com>
2015-04-26 21:43:25 -0400

    remove print info from sync.c

--- fs/sync.c
+++ fs/sync.c
@@ -282,11 +282,6 @@ no_async:
 		fput(file);
 
 		fsync_diff = ktime_sub(ktime_get(), fsync_t);
-		if (ktime_to_ms(fsync_diff) >= 5000) {
-			pr_info("VFS: %s pid:%d(%s)(parent:%d/%s) takes %lld ms to fsync %s.\n", __func__,
-				current->pid, current->comm, current->parent->pid, current->parent->comm,
-				ktime_to_ms(fsync_diff), path);
-		}
 	}
 
 	return ret;
