77a796b214d080fbc7623160113ce33161e8302e
pattyboi <patpat1325179@gmail.com>
2015-05-10 17:49:06 -0400

    update random.c and the hw random generator,import isaac,all thx to decimal i grabbed it all from his git

--- kernel/irq/handle.c
+++ kernel/irq/handle.c
@@ -133,7 +133,7 @@ irqreturn_t
 handle_irq_event_percpu(struct irq_desc *desc, struct irqaction *action)
 {
 	irqreturn_t retval = IRQ_NONE;
-	unsigned int random = 0, irq = desc->irq_data.irq;
+	unsigned int flags = 0, irq = desc->irq_data.irq;
 
 	do {
 		irqreturn_t res;
@@ -161,7 +161,7 @@ handle_irq_event_percpu(struct irq_desc *desc, struct irqaction *action)
 
 			/* Fall through to add to randomness */
 		case IRQ_HANDLED:
-			random |= action->flags;
+			flags |= action->flags;
 			break;
 
 		default:
@@ -171,9 +171,6 @@ handle_irq_event_percpu(struct irq_desc *desc, struct irqaction *action)
 		retval |= res;
 		action = action->next;
 	} while (action);
-
-	if (random & IRQF_SAMPLE_RANDOM)
-		add_interrupt_randomness(irq);
 
 	if (!noirqdebug)
 		note_interrupt(irq, desc, retval);
