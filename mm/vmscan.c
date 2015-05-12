512f175cb1bb90cad1ce102318ff405049f45869
pattyboi <patpat1325179@gmail.com>
2015-05-11 21:03:29 -0400

    dynamically change swappiness for swap/no swap builds

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -132,7 +132,12 @@ struct mem_cgroup_zone {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
+#ifdef CONFIG_SWAP
 int vm_swappiness = 60;
+#else
+int vm_swappiness = 0;
+#endif
+
 long vm_total_pages;	/* The total number of pages which the VM controls */
 
 static LIST_HEAD(shrinker_list);
